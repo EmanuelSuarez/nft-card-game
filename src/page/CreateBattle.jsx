0e74f74d690373a9477cac86f21c815c693cf7ec
ëmanuelsuarez¨ <braianesuarez@gmail.com>
2022-11-14 04:12:01 -0300

    home, create-battle and join-battle pages

---
+++ src/page/CreateBattle.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PageHOC, CustomButton, CustomInput, GameLoad } from '../components';
+import styles from '../styles';
+import { useGlobalContext } from '../context';
+
+const CreateBattle = () => {
+  const { contract, battleName, setBattleName } = useGlobalContext();
+  const [ waitBatle, setWaitBatle ] = useState(true);
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    if(!battleName || !battleName.trim()) return null;
+
+    setWaitBatle(true)
+
+    try {
+      await contract.CreateBattle(battleName);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+
+  return (
+    <>
+
+      {waitBatle && <GameLoad/>}
+
+
+    <div className='flex flex-col mb-5'>
+      <CustomInput
+      label="Battle"
+      placeholder="Enter Battle Name"
+      value={battleName}
+      handleValueChange={setBattleName}
+      />
+
+      <CustomButton
+      title="Create Battle"
+      handleClick={handleClick}
+      restStyles='mt-6'
+      />
+    </div>
+
+    <p className={styles.infoText} onClick={() => navigate('/join-battle')} >Or Join already existing batle</p>
+    </>
+  )
+};
+
+export default PageHOC(
+    CreateBattle,
+  <>Create <br /> a new battle</>,
+  <>Create your own battle and wait for other players to join you</>
+);
